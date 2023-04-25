9d255a764d3463b4f2104be77176dfdb61dec873
Joeyy485 <125569895+Joeyy485@users.noreply.github.com>
2023-04-25 00:39:52 +0000

    Todo List Project

---
+++ src/js/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoForm from "./component/TodoForm";
+import TodoList from "./component/TodoList";
+
+function App(){
+    return (
+        <div className='todo-app'>
+         <TodoList />
+        </div>
+    )
+ }
+export default App;
